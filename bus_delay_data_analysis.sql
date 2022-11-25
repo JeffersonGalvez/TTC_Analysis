b01a699a288fae2071d634b53ed403c30a31aa62
Jefferson Galvez <101986165+JeffersonGalvez@users.noreply.github.com>
2022-11-25 23:03:40 +0000

    Add files via upload

--- bus_delay_data_analysis.sql
+++ bus_delay_data_analysis.sql
@@ -2,37 +2,43 @@
 -- loading database
 USE ttc_sql_project;
 
--- tables to analyze
-SELECT *
-FROM bus_delay
-LIMIT 500;
+-- what is the date range of this data frame (df)?
+SELECT MIN(Date) AS earlieast_date 
+	, MAX(Date) AS latest_date
+FROM bus_delay;
+-- df spans fron 01 Jan 2022 to 30 Sep 2022
 
 -- How many different routes are in the bus_delay df?
 SELECT COUNT(DISTINCT Route) AS distinct_count_route
-FROM bus_delay;
+FROM bus_delay; 
+-- 219 unique bus routes
 
--- Which bus route is most prone to delays?
+-- which bus route is most prone to delays?
 SELECT Route
 	, COUNT(Incident) AS count_incident
 FROM bus_delay
 GROUP BY Route
 ORDER BY count_incident DESC
 LIMIT 1;
+-- route 36 had the most delays at 1 473 
 
--- What are the different forms/causes of bus delay, in descending order?
+-- what are the different forms/causes of bus delay, in descending order?
 SELECT Incident
 	, COUNT(Incident) AS count_incident
 FROM bus_delay
 GROUP BY Incident
 ORDER BY count_incident DESC;
+-- 'Operations - Operator' was the most common for of delay, at 15 929,
+-- followed by 'Mechanical' at 12 242 and 'Collision - TTC' at 2 472
 
--- What percentage of all bus delays are due to the top 3 causes?
+-- what percentage of all bus delays are due to the top 3 causes?
 SELECT (SUM(Incident = 'Operations - Operator')/COUNT(*)) * 100 AS percent_operations
 	, (SUM(Incident = 'Mechanical')/COUNT(*)) * 100 AS percent_mechanical
     , (SUM(Incident = 'Collision - TTC')/COUNT(*)) * 100 AS percent_collision
 FROM bus_delay;
+-- 'Operations - Operator' = 36.6%, 'Mechanical' = 28.1% and 'Collision - TTC' = 5.7%
 
--- What are each bus routes most common form of delay?
+-- what are each bus routes most common form of delay?
 SELECT Route
 	, Incident
 FROM
@@ -49,6 +55,7 @@ FROM
 	, Incident
 	) T
 WHERE rn=1;
+-- the resulting information of this query is best displayed when visualized
 
 -- which month has the most bus delays, in descending order?
 SELECT MONTHNAME(Date) AS month
@@ -56,10 +63,12 @@ SELECT MONTHNAME(Date) AS month
 FROM bus_delay
 GROUP BY MONTHNAME(Date)
 ORDER BY count_incident DESC;
+-- August is the most bus delay-prone month at 6 001 and February is the least at 3 885
 
 -- what percentage of bus delays occurred within August, the most delay-prone month?
 SELECT (SUM(MONTHNAME(Date)= 'August')/COUNT(*)) * 100 AS percent_Aug_delays
 FROM bus_delay;
+-- August accounted for 13.8% of all bus delays 
 
 -- what hour of the day are bus delays most likely to occur? By number of delays
 SELECT HOUR(Time) AS hour_of_day
@@ -67,6 +76,7 @@ SELECT HOUR(Time) AS hour_of_day
 FROM bus_delay
 GROUP BY HOUR(Time)
 ORDER BY count_incident DESC;
+-- 15:00 and 16:00 hold the most bus delays at 3 329 and 3 269, respectively
 
 -- what is the average bus delay in minutes, by day of the week?
 SELECT Day
@@ -74,15 +84,23 @@ SELECT Day
 FROM bus_delay
 GROUP BY Day
 ORDER BY avg_delay_minutes DESC;
+-- Sun = 22.0 min, Sat = 21.2 min, Wed = 19.6 min, Mon = 19.6 min, Fri = 18.7 min, Thu = 18.5 and Tue = 17.6 min
 
 -- what is the average bus delay and the average scheduled gap in minutes, by route? And what is the difference between these two averages?
+-- what effect do the delays have on on the sceduled number of buses?
 SELECT Route
 	, ROUND(AVG(delay_minutes), 1) AS avg_delay_minutes
 	, ROUND(AVG(gap_minutes), 1) AS avg_gap_minutes
-    , ROUND(AVG(gap_minutes), 1) - ROUND(AVG(delay_minutes), 1) AS delay_gap_difference
+    , ROUND(AVG(gap_minutes), 1) - ROUND(AVG(delay_minutes), 1) AS delay_gap_minutes
+    , ROUND(12 / (ROUND(AVG(gap_minutes), 1) / 60), 1) AS scheduled_no_12h
+    , ROUND(12 / ((ROUND(AVG(delay_minutes)) + ROUND(AVG(gap_minutes))) / 60), 1) AS effective_no_12h
+    , (ROUND(12 / (ROUND(AVG(gap_minutes), 1) / 60), 1)) - (ROUND(12 / ((ROUND(AVG(delay_minutes)) + ROUND(AVG(gap_minutes))) / 60), 1)) AS loss_no_buses
 FROM bus_delay
 GROUP BY Route
-ORDER BY delay_gap_difference DESC;
+ORDER BY loss_no_buses DESC;
+-- route 306 has the largest delay-to-gap difference at 30 min, with an average delay of 30 min and a scheduled delay of 60 min
+-- this means that in a 12 h period, route 306 is effectively delivering 8 buses rather than the scheduled 12, an effective loss of 4 scheduled runs
+-- however, route 600 has the greatest overall number of effective loss of 47.4 scheduled runs in a 12 h period
 
 -- which vehicle has the most 'Mechanical' bus delays?
 SELECT DISTINCT Vehicle
@@ -91,3 +109,5 @@ FROM bus_delay
 WHERE Incident = 'Mechanical'
 GROUP BY Vehicle
 ORDER BY count_mechanical_incident DESC;
+-- assuming bus (vehicle) no. 0 is real, it has experienced the most mechanical-related elays of 41
+-- followed by bus (vehcile) no. 9037 at 24 
